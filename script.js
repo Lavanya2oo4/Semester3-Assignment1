6cf7d914a3cf2112c0e0634b466beb521dd18721
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-01-12 13:43:44 -0500

    Added Code to Delete Row from start when button clicked ad modified code to insert new row to remember the last row it created

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let currentRowCount;
+let currentRowCount=document.getElementsByClassName("tableRow").length
+
+
+
+
 //executed when insert button is clicked,Inserts a new row at end
+
 function insert(){
 let table=document.getElementById("table")
 
@@ -7,15 +12,23 @@ let table=document.getElementById("table")
 let newRow=table.insertRow(-1)
 newRow.classList.add("tableRow")
 
-let rowCount=document.getElementsByClassName("tableRow").length
-currentRowCount=rowCount
+let rowCount=currentRowCount+1
 // console.log(currentRowCount)
 
 
 let c1=newRow.insertCell(0)
 let c2=newRow.insertCell(1)
 
-c1.innerHTML=`Row ${currentRowCount} Col 1`
-c2.innerHTML=`Row ${currentRowCount} Col 2`
+c1.innerHTML=`Row ${rowCount} Col 1`
+c2.innerHTML=`Row ${rowCount} Col 2`
+
+currentRowCount+=1
+}
+
+
+//deletes Row From Start
+function delStart(){
+    let table=document.getElementById("table")
+    table.deleteRow(0)
 
 }
